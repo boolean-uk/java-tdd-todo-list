c7915d2f9b4335a7379cbdf1a97fe090de837df2
Aidan van Geest <aidanvangeest@gmail.com>
2023-05-09 16:34:29 +0200

    Completed first half of source code

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,57 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class TodoList {
+//    HashMap <String, Boolean> tasks = new HashMap<>();
+    ArrayList<Task> tasks = new ArrayList<>();
+    private boolean _searchTask(String task) {
+        for (Task t: tasks) {
+            if (t.name.equals(task)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String searchTask(String task) {
+        return (_searchTask(task) ? "The task was found." : "The task could not be found.");
+    }
+
+    public void addTask(Task task) {
+        if(_searchTask(task.name)) {
+            System.out.println("This task already exists. Please use a new name if you want to create a new task, or use the updateTask method");
+        } else {
+            this.tasks.add(task);
+        }
+    }
+
+    public String displayAllTasks() {
+        if (this.tasks.isEmpty()) {
+            return "No tasks were found.";
+        }
+
+        String result = "";
+        for (Task task: tasks) {
+            result += task.name + ", " + ((task.completed) ? "complete" : "incomplete") + "\n";
+        }
+        return result;
+    }
+
+    public boolean updateTask(String task) {
+        if(!_searchTask(task)) {
+            return false;
+        }
+
+        for (Task t : this.tasks) {
+            if (t.name.equals(task)) {
+                t.completed = !t.completed;
+                break;
+            }
+        }
+        return true;
+    }
+
 
 }
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -10,4 +10,51 @@ class TodoListTest {
         Assertions.assertEquals("Hello", hello);
         Assertions.assertNotEquals("Goodbye", hello);
     }
+
+    @Test
+    public void testSearchTask() {      //Testing this public method gives us assurance that the private method it calls also works correctly
+        TodoList list = new TodoList();
+        String testResult = "The task could not be found.";
+        Assertions.assertEquals(testResult, list.searchTask("Wash dishes"));
+
+        testResult = "The task was found.";
+        list.tasks.add(new Task("Wash dishes", false));
+        Assertions.assertEquals(testResult, list.searchTask("Wash dishes"));
+    }
+
+    @Test
+    public void testAddTask() {
+        TodoList list = new TodoList();
+        Task testTask = new Task("Go shopping", false);
+        list.addTask(testTask);
+        Assertions.assertTrue(list.tasks.contains(testTask));
+    }
+
+    @Test
+    public void testDisplayAllTasks() {
+        TodoList list = new TodoList();
+        String testResult = "No tasks were found.";
+        Assertions.assertEquals(testResult, list.displayAllTasks());
+
+        Task task1 = new Task("Wash dishes", false);
+        Task task2 = new Task("Go swimming", true);
+        list.tasks.add(task1);
+        list.tasks.add(task2);
+        testResult = "Wash dishes, incomplete\nGo swimming, complete\n";
+        Assertions.assertEquals(testResult, list.displayAllTasks());
+    }
+
+    @Test
+    public void testUpdateTask() {
+        TodoList list = new TodoList();
+        Assertions.assertFalse(list.updateTask("Slide down fireman pole"));
+
+        Task task1 = new Task("Jump on bed", false);
+        list.tasks.add(task1);
+        list.updateTask(task1.name);
+        Assertions.assertTrue(list.tasks.get(list.tasks.indexOf(task1)).completed);
+    }
+
+
+
 }
