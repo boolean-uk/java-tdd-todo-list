da827fa540cef506f810a6a834ffa5324d47859c
Rose Breedveld <rosebreedveld@hotmail.com>
2023-05-09 11:05:15 -0400

    Test all tasks

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -14,4 +14,5 @@ public class TodoList {
         this.tasks.add(newTask);
     }
 
+
 }
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -6,10 +6,23 @@ import org.junit.jupiter.api.Test;
 class TodoListTest {
 
     @Test
-    public void returnTrueIfTaskIsAddedToList() {
+    public void testReturnTrueIfTaskIsAddedToList() {
         TodoList todoList = new TodoList();
         Task newTask = new Task("Laundry");
         todoList.tasks.add(newTask);
         Assertions.assertTrue(todoList.tasks.contains(newTask));
+    }
+    @Test
+    public void testReturnAllTasks() {
+        TodoList todoList = new TodoList();
+        Task newTask = new Task("Laundry");
+        Task newTask2 = new Task("Cleaning");
+        Task newTask3 = new Task("Reading");
+        todoList.tasks.add(newTask);
+        todoList.tasks.add(newTask2);
+        todoList.tasks.add(newTask3);
+        Assertions.assertEquals("Laundry", todoList.tasks.get(0).name);
+        Assertions.assertEquals("Cleaning", todoList.tasks.get(1).name);
+        Assertions.assertEquals("Reading", todoList.tasks.get(2).name);
     }
 }
