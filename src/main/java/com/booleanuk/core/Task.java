344be635a1110994accf162e125c20e79b201491
Jan Rusak <jrusak001@gmail.com>
2023-07-10 13:28:04 +0200

    Added Task class

---
+++ src/main/java/com/booleanuk/core/Task.java
@@ -0,0 +1,11 @@
+package com.booleanuk.core;
+
+public class Task {
+    public String text;
+    public boolean isCompleted;
+
+    public Task(String text) {
+        this.text = text;
+        this.isCompleted = false;
+    }
+}
