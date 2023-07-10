62d0c37b76a1dad46245b27b6f0f912fbcac4679
Anna Sperkach <annsperkach@gmail.com>
2023-07-10 15:02:05 +0300

    Extension task: Add domain-model-extension and classes TaskCreator, TaskStatus and Task

---
+++ src/test/java/com/booleanuk/extension/TaskCreatorTest.java
@@ -0,0 +1,8 @@
+package com.booleanuk.extension;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+
+public class TaskCreatorTest {
+    }
