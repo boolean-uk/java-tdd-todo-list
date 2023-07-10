42e2abdef84ebb34d4bd9bd68c4e55a82c12db4d
himok <himok.pk@gmail.com>
2023-07-10 16:16:35 +0200

    Adding addTask method

--- src/main/java/com/booleanuk/extension/TodoList.java
+++ src/main/java/com/booleanuk/extension/TodoList.java
@@ -3,6 +3,12 @@ package com.booleanuk.extension;
 import java.util.HashMap;
 
 public class TodoList {
-    private final int idCounter = 0;
+    private int idCounter = 0;
     final HashMap<Integer, Task> tasks = new HashMap<>();
+
+    public void addTask(String text) {
+        Task newTask = new Task(text);
+        tasks.put(idCounter, newTask);
+        idCounter++;
+    }
 }
