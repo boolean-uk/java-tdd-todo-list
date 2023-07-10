35dcca2cdb6c31001dfee603d2f5356719d25bff
Szkaradd <szkaradekmikolaj@gmail.com>
2023-07-10 14:00:12 +0200

    created models, tests for add and show working

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,24 @@
 package com.booleanuk.core;
 
-public class TodoList {
+import java.util.ArrayList;
+import java.util.List;
 
+public class TodoList {
+    private List<Task> tasks = new ArrayList<>();
+    public boolean add(Task task) {
+        if (tasks.contains(task)) return false;
+        tasks.add(task);
+        return true;
+    }
+
+    public void showTasks() {
+        tasks.forEach(System.out::println);
+    }
+
+    @Override
+    public String toString() {
+        return "TodoList{" +
+                "tasks=" + tasks +
+                '}';
+    }
 }
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -1,13 +1,65 @@
 package com.booleanuk.core;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 class TodoListTest {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private TodoList todoList;
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+        todoList = new TodoList();
+    }
+
+    private void addSomeTasks(TodoList todoList) {
+        Task task1 = new Task("Go shopping");
+        Task task2 = new Task("Clean your room");
+        todoList.add(task1);
+        todoList.add(task2);
+    }
+
     @Test
     public void exampleTest() {
         String hello = "Hello";
         Assertions.assertEquals("Hello", hello);
         Assertions.assertNotEquals("Goodbye", hello);
+    }
+
+    @Test
+    public void testShowTasksOutput() {
+        addSomeTasks(todoList);
+        todoList.showTasks();
+        String expected = "Task{" +
+                "id=" + 0 +
+                ", description='" + "Go shopping" + '\'' +
+                ", status=" + "INCOMPLETE" +
+                '}';
+        expected += System.lineSeparator();
+        expected += "Task{" +
+                "id=" + 1 +
+                ", description='" + "Clean your room" + '\'' +
+                ", status=" + "INCOMPLETE" +
+                '}';
+        Assertions.assertEquals(expected.trim(), outputStreamCaptor.toString()
+                .trim());
+    }
+
+    @Test
+    public void addTaskShouldReturnTrue() {
+        // Execute
+        boolean added1 = todoList.add(new Task("test1"));
+        boolean added2 = todoList.add(new Task("test2"));
+
+        // Verify
+        Assertions.assertTrue(added1);
+        Assertions.assertTrue(added2);
     }
 }
