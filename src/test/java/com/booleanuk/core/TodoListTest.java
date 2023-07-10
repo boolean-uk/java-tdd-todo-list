4b1732451d32a7b2b3ca1f21cfe92b9235fb8616
Rafal Halama <rafau.halama@gmail.com>
2023-07-10 13:42:23 +0200

    first test created

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -1,7 +1,6 @@
 package com.booleanuk.core;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class TodoListTest {
     @Test
@@ -10,4 +9,24 @@ class TodoListTest {
         Assertions.assertEquals("Hello", hello);
         Assertions.assertNotEquals("Goodbye", hello);
     }
+
+    private static TodoList todolist;
+    private static Task task;
+
+    @BeforeAll
+    public static void createTasks(){
+        task = new Task("Do homework", false);
+    }
+    @BeforeEach
+    public void create(){
+        todolist = new TodoList();
+    }
+    @Test
+    public void addTask(){
+        boolean result1 = todolist.addTask(task);
+        Assertions.assertTrue(result1);
+        boolean result2 = todolist.addTask(task);
+        Assertions.assertFalse(result2);
+    }
+
 }
