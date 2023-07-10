2ef1398c8727019bc9ebceb48454f8158642e67c
AdiletCoder <adiletnasirov@gmail.com>
2023-07-10 15:21:57 +0200

    First task

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,20 @@
 package com.booleanuk.core;
 
-public class TodoList {
+import java.util.ArrayList;
 
+public class TodoList {
+    public String task;
+    public boolean status;
+
+    ArrayList<TodoList> tasks = new ArrayList<TodoList>();
+
+    public TodoList(String task, boolean status) {
+        this.task = task;
+        this.status = status;
+    }
+
+    public String add(String task, boolean status){
+        tasks.add(new TodoList(task,status));
+        return "Task added to to-do list";
+    }
 }
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -10,4 +10,10 @@ class TodoListTest {
         Assertions.assertEquals("Hello", hello);
         Assertions.assertNotEquals("Goodbye", hello);
     }
+
+    @Test
+    public void addTest(){
+        TodoList todo = new TodoList("First task", false);
+        Assertions.assertEquals("Task added to to-do list", todo.add("Second task", false) );
+    }
 }
