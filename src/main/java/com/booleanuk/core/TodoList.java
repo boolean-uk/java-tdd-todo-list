91088e5214e1a51314c7b8bb3950265de35610e9
Kornel <korneldabrowski@gmail.com>
2023-07-10 13:39:25 +0200

    core 1-6 and tests

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,69 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class TodoList {
+
+    private HashMap<String, Boolean> todoList = new HashMap<String, Boolean>();
+
+    public HashMap<String, Boolean> getTodoList() {
+        return todoList;
+    }
+
+    // I want to add tasks to my todo list.
+    public void add(String todo) {
+        todoList.put(todo, false);
+    }
+
+    // I want to see all the tasks in my todo list.
+    public void list() {
+        for (String key : todoList.keySet()) {
+            System.out.println(key);
+        }
+    }
+
+    // I want to change the status of a task between incomplete and complete.
+    public boolean toggle(String todo) {
+        if (todoList.containsKey(todo)) {
+            todoList.put(todo, !todoList.get(todo));
+            return true;
+        }
+        return false;
+    }
+
+    // I want to be able to get only the complete tasks.
+    public HashMap<String, Boolean> getComplete() {
+        HashMap<String, Boolean> complete = new HashMap<String, Boolean>();
+
+        for (String key : todoList.keySet()) {
+            if (todoList.get(key)) {
+                complete.put(key, todoList.get(key));
+            }
+        }
+        return complete;
+    }
+
+    // I want to be able to get only the incomplete tasks.
+    public HashMap<String, Boolean> getIncomplete() {
+        HashMap<String, Boolean> inComplete = new HashMap<String, Boolean>();
+
+        for (String key : todoList.keySet()) {
+            if (!todoList.get(key)) {
+                inComplete.put(key, todoList.get(key));
+            }
+        }
+        return inComplete;
+    }
+
+    // I want to search for a task and receive a message that says it wasn't found
+    // if it doesn't exist.
+
+    public String search(String todo) {
+        if (todoList.containsKey(todo)) {
+            return todo;
+        }
+        return "Task not found";
+    }
 
 }
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -10,4 +10,108 @@ class TodoListTest {
         Assertions.assertEquals("Hello", hello);
         Assertions.assertNotEquals("Goodbye", hello);
     }
+
+    @Test
+    public void addTest() {
+
+        TodoList mytodo = new TodoList();
+
+        mytodo.add("Test");
+
+        Assertions.assertTrue(mytodo.getTodoList().containsKey("Test"));
+
+    }
+
+    @Test
+    public void listTest() {
+
+        TodoList mytodo = new TodoList();
+
+        mytodo.add("Test");
+        mytodo.add("Test2");
+        mytodo.add("Test3");
+        mytodo.add("Test4");
+        mytodo.add("Test5");
+
+        Assertions.assertEquals(5, mytodo.getTodoList().size());
+    }
+
+    @Test
+    public void testToggling() {
+
+        TodoList mytodo = new TodoList();
+
+        mytodo.add("Test");
+
+        Assertions.assertFalse(mytodo.getTodoList().get("Test"));
+
+        mytodo.toggle("Test");
+
+        Assertions.assertTrue(mytodo.getTodoList().get("Test"));
+    }
+
+    @Test
+    public void testGetComplete() {
+
+        TodoList mytodo = new TodoList();
+
+        mytodo.add("Test");
+        mytodo.add("Test2");
+        mytodo.add("Test3");
+        mytodo.add("Test4");
+        mytodo.add("Test5");
+
+        mytodo.toggle("Test");
+        mytodo.toggle("Test3");
+        mytodo.toggle("Test5");
+
+        Assertions.assertEquals(3, mytodo.getComplete().size());
+    }
+
+    @Test
+    public void testGetIncomplete() {
+
+        TodoList mytodo = new TodoList();
+
+        mytodo.add("Test");
+        mytodo.add("Test2");
+        mytodo.add("Test3");
+        mytodo.add("Test4");
+        mytodo.add("Test5");
+
+        mytodo.toggle("Test");
+        mytodo.toggle("Test3");
+        mytodo.toggle("Test5");
+
+        Assertions.assertEquals(2, mytodo.getIncomplete().size());
+    }
+
+    @Test
+    public void testGetMessageIfFound() {
+
+        TodoList mytodo = new TodoList();
+
+        mytodo.add("Test");
+        mytodo.add("Test2");
+        mytodo.add("Test3");
+        mytodo.add("Test4");
+        mytodo.add("Test5");
+
+        Assertions.assertEquals(mytodo.search("Test"), "Test");
+    }
+
+    @Test
+    public void testGetMessageIfNotFound() {
+
+        TodoList mytodo = new TodoList();
+
+        mytodo.add("Test");
+        mytodo.add("Test2");
+        mytodo.add("Test3");
+        mytodo.add("Test4");
+        mytodo.add("Test5");
+
+        Assertions.assertEquals(mytodo.search("Test6"), "Task not found");
+    }
+
 }
