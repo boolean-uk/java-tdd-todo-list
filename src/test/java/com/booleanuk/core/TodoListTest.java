278dc33f096629121b3e6afa3f2fa48fa59de86b
Kamil Kulak <kamil.kulak@talan.com>
2023-07-10 14:16:24 +0200

    Refactored add method

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -1,13 +1,29 @@
 package com.booleanuk.core;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TodoListTest {
+    TodoList todoList;
+    Task task;
+
+    @BeforeEach
+    public void setup() {
+        todoList = new TodoList();
+        task = new Task();
+    }
+
+
     @Test
-    public void exampleTest() {
-        TodoList todoList = new TodoList();
-        Task task = new Task();
+    public void addTaskReturnTrue() {
         Assertions.assertTrue(todoList.add(task));
+    }
+
+    @Test
+    public void addTaskReturnFalse() {
+        todoList.add(task);
+        Assertions.assertFalse(todoList.add(task));
     }
 }
