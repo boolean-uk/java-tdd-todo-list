e4bc6373764170be5e656f8ce58909881ae1174e
jakublaba <jakub.maciej.laba@gmail.com>
2023-07-10 14:21:54 +0200

    Refactor Task to make status mutable

--- src/main/java/com/booleanuk/core/Task.java
+++ src/main/java/com/booleanuk/core/Task.java
@@ -1,10 +1,34 @@
 package com.booleanuk.core;
 
-public record Task(
-        String name,
-        TaskStatus status
-) {
+
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
+public class Task {
+    private final String name;
+    private TaskStatus status;
+
+    private Task(String name, TaskStatus status) {
+        this.name = name;
+        this.status = status;
+    }
+
     public static Task of(String name) {
         return new Task(name, TaskStatus.InProgress);
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public TaskStatus status() {
+        return status;
+    }
+
+    public void toggleStatus() {
+        switch (status) {
+            case InProgress -> status = TaskStatus.Complete;
+            case Complete -> status = TaskStatus.InProgress;
+        }
     }
 }
