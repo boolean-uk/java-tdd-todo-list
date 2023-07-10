6b23bdb830d2083b39358d8c094bdca6ecef48e6
PawelCagara <pawel.cagara@talan.com>
2023-07-10 15:26:12 +0200

    tested method addTest() - checking if Map contains task

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,22 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TodoList {
+
+    Map<String, String> toDoList;
+
+    public TodoList() {
+        toDoList = new HashMap<>();
+    }
+
+    public Map<String, String> getToDoList() {
+        return toDoList;
+    }
+
+    public void add(String task) {
+        toDoList.put(task, "incomplete");
+    }
 
 }
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -3,11 +3,26 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class TodoListTest {
     @Test
     public void exampleTest() {
         String hello = "Hello";
         Assertions.assertEquals("Hello", hello);
         Assertions.assertNotEquals("Goodbye", hello);
+    }
+
+    TodoList todoList;
+
+    public TodoListTest() {
+        this.todoList = new TodoList();
+    }
+    @Test
+    public void addTest() {
+        todoList.add("Running for 5km");
+
+        Assertions.assertTrue(todoList.getToDoList().containsKey("Running for 5km"));
     }
 }
