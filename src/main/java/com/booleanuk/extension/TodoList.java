d2b5e551655ce562a0b872a38da7aedd219be2fa
jakublaba <jakub.maciej.laba@gmail.com>
2023-07-10 15:05:40 +0200

    Add domain model (extension)

---
+++ src/main/java/com/booleanuk/extension/TodoList.java
@@ -0,0 +1,27 @@
+package com.booleanuk.extension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class TodoList {
+    private final List<Task> tasks = new ArrayList<>();
+
+    public void addTask(String name) {
+        // TODO
+    }
+
+    public void updateTaskName(long id, String newName) {
+        // TODO
+    }
+
+
+    public void toggleTaskStatus(long id) {
+        // TODO
+    }
+
+    public Optional<Task> getTask(long id) {
+        // TODO
+        return null;
+    }
+}
