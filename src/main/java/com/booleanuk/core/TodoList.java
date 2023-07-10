3c5e4b5d81ed0ebdba73c5aec026230d55eeca77
Kamil Kulak <kamil.kulak@talan.com>
2023-07-10 14:10:57 +0200

    green stage, adding task to list works

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,6 +1,19 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
 public class TodoList {
+    List<Task> tasks = new ArrayList<>();
 
-
+    public boolean add(Task task) {
+        if (tasks.contains(task)) {
+            return false;
+        }
+        else {
+            tasks.add(task);
+            return true;
+        }
+    }
 }
