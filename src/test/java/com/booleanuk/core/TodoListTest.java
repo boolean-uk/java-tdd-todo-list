c66bbfc7b885708cde58bafe61c3a7be3806e0b8
Bartlomiej Walczak <vbawalc@gmail.com>
2023-07-10 14:27:46 +0200

    tests

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -3,6 +3,7 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -62,6 +63,30 @@ class TodoListTest {
 
         Assertions.assertFalse(newTask.isComplete());
 
+    }
+    @Test
+    public void testGetCompleteTasks() {
+        String taskDescription1 = "Walk the dog";
+        String taskDescription2 = "Walk the dog2";
+        TodoList todolist = new TodoList();
+        Task newTask = todolist.addTask(taskDescription1);
+        Task newTask2 = todolist.addTask(taskDescription2);
+
+        newTask.changeTaskStatus();
+        ArrayList<Task> result = todolist.getCompleteTasks();
+        Assertions.assertEquals(result.size(), 1);
+    }
+    @Test
+    public void testGetIncompleteTasks() {
+        String taskDescription1 = "Walk the dog";
+        String taskDescription2 = "Walk the dog2";
+        TodoList todolist = new TodoList();
+        Task newTask = todolist.addTask(taskDescription1);
+        Task newTask2 = todolist.addTask(taskDescription2);
+
+        newTask.changeTaskStatus();
+        ArrayList<Task> result = todolist.getIncompleteTasks();
+        Assertions.assertEquals(result.size(), 1);
     }
 
 }
