d0fcf1ff7e0715322c64fa1c5d50aacbc15ea289
Michal Antoniuk <michal.antoniuk@talan.com>
2023-07-10 16:20:18 +0200

    Written green test for extension: updating task name

---
+++ src/main/java/com/booleanuk/extension/Task.java
@@ -0,0 +1,65 @@
+package com.booleanuk.extension;
+
+import java.time.LocalDateTime;
+
+public class Task{
+    private int id;
+    private String name;
+    private boolean isComplete;
+    private LocalDateTime createTime;
+
+
+
+    public Task(String name, boolean isComplete, LocalDateTime createTime) {
+        this.name = name;
+        this.isComplete = isComplete;
+        this.createTime = createTime;
+    }
+
+    public boolean toggleStatus(){
+        this.isComplete = !this.isComplete;
+        return this.isComplete;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", status=" + isComplete +
+                ", createTime=" + createTime +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.isComplete = complete;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/booleanuk/extension/TodoList.java
@@ -0,0 +1,78 @@
+package com.booleanuk.extension;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class TodoList {
+
+    private List<Task> tasks = new ArrayList<>();
+    private int lastId = 0;
+
+
+    public int add(Task task) {
+        task.setId(++lastId);
+        tasks.add(task);
+        return task.getId();
+    }
+
+    public List<String> getAll() {
+        return tasks.stream().map(Task::toString).toList();
+    }
+
+
+    public boolean toggleTaskStatus(int id) {
+        return getById(id).toggleStatus();
+    }
+
+    public Task getById(int id) {
+        return tasks.stream().filter(task -> task.getId() == id).findFirst().orElseThrow();
+    }
+
+    public List<Task> getTasksAscending() {
+        List<Task> sortedTasks = new ArrayList<>(tasks);
+
+        sortedTasks.sort(Comparator.comparing(Task::getName));
+
+        return sortedTasks;
+
+    }
+
+    public List<Task> getTasksDescending() {
+        List<Task> sortedTasks = new ArrayList<>(tasks);
+
+        sortedTasks.sort(Comparator.comparing(Task::getName).reversed());
+
+        return sortedTasks;
+    }
+
+    public List<Task> getCompleteTasks() {
+        return tasks.stream().filter(Task::isComplete).toList();
+    }
+
+    public List<Task> getIncompleteTasks() {
+        return tasks.stream().filter(task -> !task.isComplete()).toList();
+    }
+
+    public String taskExists(int id) {
+        if (tasks.stream().anyMatch(task -> task.getId() == id)) {
+            return String.format("Task with id %d exists in todo list.", id);
+        }
+        return String.format("Task with id %d doesn't exist in todo list.", id);
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public boolean removeTask(int id) {
+        return tasks.removeIf(task -> task.getId() == id);
+    }
+
+
+    public boolean updateTaskName(int id, String updatedName) {
+        Task task = getById(id);
+        task.setName(updatedName);
+        return task.getName().equals(updatedName);
+    }
+}
