f8305492b9e8dd41adb0ed9518c7a7ead2f1b415
Kamil Kulak <kamil.kulak@talan.com>
2023-07-10 15:04:50 +0200

    Red stage Incomplete tasks

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -7,12 +7,14 @@ import java.util.List;
 
 class TodoListTest {
     TodoList todoList;
-    Task task;
+    Task taskComplete;
+    Task taskIncomplete;
 
     @BeforeEach
     public void setup() {
         todoList = new TodoList();
-        task = new Task(Status.COMPLETE,"Dishes");
+        taskComplete = new Task(Status.COMPLETE,"Dishes");
+        taskIncomplete = new Task(Status.INCOMPLETE,"Dishes");
     }
 
     @AfterEach
@@ -22,13 +24,13 @@ class TodoListTest {
 
     @Test
     public void addTaskReturnTrue() {
-        Assertions.assertTrue(todoList.add(task));
+        Assertions.assertTrue(todoList.add(taskComplete));
     }
 
     @Test
     public void addTaskReturnFalse() {
-        todoList.add(task);
-        Assertions.assertFalse(todoList.add(task));
+        todoList.add(taskComplete);
+        Assertions.assertFalse(todoList.add(taskComplete));
     }
 
     @Test
@@ -47,11 +49,26 @@ class TodoListTest {
 
     @Test
     public void getCompleteTaskReturnList() {
-        todoList.add(task);
+        todoList.add(taskComplete);
         List<Task> result = todoList.getCompleteTasks();
 
         Assertions.assertEquals(1, result.size());
         Assertions.assertEquals(todoList.tasks.get(0).status, Status.COMPLETE);
     }
 
+    @Test
+    public void getInCompleteTaskReturnEmptyList() {
+        List<Task> result = todoList.getIncompleteTasks();
+
+        Assertions.assertEquals(0, result.size());
+    }
+
+    @Test
+    public void getInCompleteTaskReturnList() {
+        todoList.add(taskIncomplete);
+        List<Task> result = todoList.getIncompleteTasks();
+
+        Assertions.assertEquals(1, result.size());
+        Assertions.assertEquals(todoList.tasks.get(0).status, Status.INCOMPLETE);
+    }
 }
