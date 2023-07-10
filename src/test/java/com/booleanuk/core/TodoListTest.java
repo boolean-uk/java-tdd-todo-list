2a0c76f655ad75c9e84a598014f99a81b2e7e1e5
Kinga <kinga.hernet@talan.com>
2023-07-10 13:21:34 +0200

    Implemented the tests

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -10,4 +10,49 @@ class TodoListTest {
         Assertions.assertEquals("Hello", hello);
         Assertions.assertNotEquals("Goodbye", hello);
     }
+
+    @Test
+    public void testSearchTask() {
+        TodoList list = new TodoList();
+        String testResult = "The task could not be found.";
+        Assertions.assertEquals(testResult, list.searchTask("Do the laundry"));
+
+        testResult = "The task was found.";
+        list.tasks.add(new Task("Do the laundry", false));
+        Assertions.assertEquals(testResult, list.searchTask("Do the laundry"));
+    }
+
+    @Test
+    public void testAddTask() {
+        TodoList list = new TodoList();
+        Task testTask = new Task("Do the dishes", false);
+        list.addTask(testTask);
+        Assertions.assertTrue(list.tasks.contains(testTask));
+    }
+
+    @Test
+    public void testDisplayAllTasks() {
+        TodoList list = new TodoList();
+        String testResult = "No tasks were found.";
+        Assertions.assertEquals(testResult, list.displayAllTasks());
+
+        Task task1 = new Task("Do the laundry", false);
+        Task task2 = new Task("Do the grocery shopping", true);
+        list.tasks.add(task1);
+        list.tasks.add(task2);
+        testResult = "Do the laundry, incomplete\n Do the grocery shopping, complete\n";
+        Assertions.assertEquals(testResult, list.displayAllTasks());
+    }
+
+    @Test
+    public void testUpdateTask() {
+        TodoList list = new TodoList();
+        Assertions.assertFalse(list.updateTask("Clean the window"));
+
+        Task task1 = new Task("Do the homework", false);
+        list.tasks.add(task1);
+        list.updateTask(task1.name);
+        Assertions.assertTrue(list.tasks.get(list.tasks.indexOf(task1)).completed);
+    }
+
 }
