e9e8fdfdd4262565cdb51dbe233ac2e9072855ee
Michał Klukowski <michal.klukowski@talan.com>
2023-07-10 14:34:52 +0200

    Implement sorting

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,6 +1,7 @@
 package com.booleanuk.core;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TodoList {
@@ -33,5 +34,14 @@ public class TodoList {
 
     public boolean removeTask(Task task) {
         return tasks.remove(task);
+    }
+
+    public String listTasks(String order) {
+        if (order.equals("descending"))
+            tasks.sort((task1, task2) -> task2.name.compareTo(task1.name));
+        else
+            tasks.sort((task1, task2) -> task1.name.compareTo(task2.name));
+
+        return tasks.toString();
     }
 }
