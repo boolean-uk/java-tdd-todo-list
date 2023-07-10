f10aa16ae86b998e4f2190202c3c5635de8f1bc1
jakublaba <jakub.maciej.laba@gmail.com>
2023-07-10 13:17:13 +0200

    Add domain model

---
+++ src/main/java/com/booleanuk/core/SortingOrder.java
@@ -0,0 +1,6 @@
+package com.booleanuk.core;
+
+public enum SortingOrder {
+    Descending,
+    Ascending
+}
--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,52 @@
 package com.booleanuk.core;
 
-public class TodoList {
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+public class TodoList {
+    private final List<Task> tasks = new ArrayList<>();
+
+    public void addTask(String name) {
+        // TODO
+    }
+
+    public List<Task> getAllTasks() {
+        // TODO
+        return null;
+    }
+
+    public void toggleTaskStatus(String name) {
+        // TODO
+    }
+
+    public List<Task> getAllTasksByStatus(TaskStatus status) {
+        // TODO
+        return null;
+    }
+
+    public Optional<Task> getOneTaskByName(String name) {
+        // TODO
+        return null;
+    }
+
+    public void removeOneTaskByName(String name) {
+        // TODO
+    }
+
+    public void removeAllTasksByName(String... names) {
+        // TODO
+    }
+
+    public List<Task> getAllTasksSorted(SortingOrder sortingOrder) {
+        // TODO
+        return null;
+    }
+
+    public record Task(
+        String name,
+        TaskStatus status
+    ) {
+    }
 }
