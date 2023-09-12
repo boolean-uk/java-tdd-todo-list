58c34410497f3002c1100f99b1458a96c1376bf1
Irene <irene.maniou@hotmail.com>
2023-09-12 17:41:45 +0300

    Irene Maniou

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -10,4 +10,17 @@ class TodoListTest {
         Assertions.assertEquals("Hello", hello);
         Assertions.assertNotEquals("Goodbye", hello);
     }
+
+    @Test
+    public void addTask() {
+        TodoList todoList = new TodoList();
+        String task = "Wash the car";
+        Assertions.assertTrue(todoList.addTask(task));
+
+        String task2 = task;
+        Assertions.assertFalse(todoList.addTask(task2));
+
+        String task3 = "Do the dishes";
+        Assertions.assertTrue(todoList.addTask(task3));
+    }
 }
