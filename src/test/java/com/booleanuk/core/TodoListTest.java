0735aa47ed438453896c041ec1fe70740b7f24ec
VeroniqueDM <veronika.dmiteva@gmail.com>
2023-09-12 16:55:25 +0200

    added tests for second method

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -12,6 +12,7 @@ class TodoListTest {
         Assertions.assertTrue(todoList.tasks.containsKey("Feed the cat"));
         Assertions.assertFalse(todoList.tasks.get("Feed the cat"));
     }
+
     @Test
     public void addTaskFailed() {
         TodoList todoList = new TodoList();
@@ -20,11 +21,27 @@ class TodoListTest {
         Assertions.assertTrue(todoList.tasks.containsKey("Feed the cat"));
         Assertions.assertFalse(todoList.addTask("Feed the cat"));
     }
+
     @Test
     public void addTaskFailsBecauseEmptyString() {
         TodoList todoList = new TodoList();
         Assertions.assertFalse(todoList.addTask("\n\r\t"));
         Assertions.assertEquals(0, todoList.tasks.size());
+    }
+
+    @Test
+    public void getTasksWhenMapNotEmpty() {
+        TodoList todoList = new TodoList();
+        String taskOne = "Feed the cat";
+        String taskTwo = "Go shopping";
+        todoList.addTask(taskOne);
+        todoList.addTask(taskTwo);
+        Assertions.assertEquals("Tasks: " + String.join(", ", todoList.tasks.keySet()), todoList.getTasks());
+    }
+    @Test
+    public void getTasksWhenMapIsEmpty() {
+        TodoList todoList = new TodoList();
+        Assertions.assertEquals("No tasks in your list!" , todoList.getTasks());
     }
 
 }
