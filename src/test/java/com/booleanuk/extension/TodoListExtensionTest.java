257fd9e7c70afd24528358363b6bb9c54b40f26c
VeroniqueDM <veronika.dmiteva@gmail.com>
2023-09-13 00:55:40 +0200

    added tests for updateTask extension

--- src/test/java/com/booleanuk/extension/TodoListExtensionTest.java
+++ src/test/java/com/booleanuk/extension/TodoListExtensionTest.java
@@ -10,10 +10,19 @@ class TodoListExtensionTest {
         TodoListExtension todoList = new TodoListExtension();
         Assertions.assertTrue(todoList.addTask("Feed the cat"));
         Assertions.assertEquals(1, todoList.tasks.size());
-        Assertions.assertEquals(("Feed the cat"),todoList.tasks.get(0).getName());
+        Assertions.assertEquals(("Feed the cat"), todoList.tasks.get(0).getName());
         Assertions.assertEquals("Task 1: Feed the cat - incomplete", todoList.getTaskById(1));
         Assertions.assertEquals("Task not found", todoList.getTaskById(2));
 
     }
 
+    @Test
+    public void testUpdateTaskName() {
+        TodoListExtension todoList = new TodoListExtension();
+        Assertions.assertTrue(todoList.addTask("Feed the cat"));
+        Assertions.assertEquals(1, todoList.tasks.size());
+        Assertions.assertTrue(todoList.updateTaskName(1, "Feed the dog"));
+        Assertions.assertFalse(todoList.updateTaskName(2, "Feed the rabbit"));
+
+    }
 }
