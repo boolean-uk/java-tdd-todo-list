66d8850fdd895fc621a27b654d306d7ab6128c6e
Esther <estherbuijnsters@gmail.com>
2023-09-12 18:12:14 +0200

    third test changeStatus passed

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -9,6 +9,7 @@ public class TodoList {
   public TodoList() {
     this.taskList = new LinkedHashMap<>();
   }
+
   public boolean addToTodoList(String task, boolean taskFinished) {
     if (this.taskList.containsKey(task)) {
       return false;
@@ -22,6 +23,15 @@ public class TodoList {
     if (this.taskList.isEmpty()) {
       return "You have no tasks on your todoList";
     }
-      return "Task: " + String.join(", ", this.taskList.keySet());
+    return "Task: " + String.join(", ", this.taskList.keySet());
+  }
+
+  public boolean changeStatus(String task, boolean newStatus) {
+    if (taskList.containsKey(task)) {
+      taskList.put(task, newStatus);
+      System.out.println("Status of task changed" + " " + taskList);
+      return true; // Status changed successfully
+    }
+    return false; // Task not found
   }
 }
