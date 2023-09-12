35d5e32ce1d71618f64fc29a85ab4aefc48eb62d
Irene <irene.maniou@hotmail.com>
2023-09-12 17:41:56 +0300

    Irene Maniou

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,22 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+
 public class TodoList {
 
+    HashMap<String, String> toDoList;
+
+    public TodoList() {
+        this.toDoList = new HashMap<>();
+    }
+
+    public boolean addTask(String task) {
+        if (toDoList.containsKey(task)) {
+            System.out.println("Task already exists");
+            return false;
+        }
+        toDoList.put(task, "incomplete");
+        System.out.println("Task added successfully");
+        return true;
+    }
 }
