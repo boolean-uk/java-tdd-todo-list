b561c2df3e25d1996025231ad9691562b09b93e4
msg <mats.gravem@no.experis.com>
2024-01-16 12:56:55 +0100

    Test for todo-list constructor and add() method

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -1,13 +1,30 @@
 package com.booleanuk.core;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 class TodoListTest {
-    @Test
-    public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
+    TodoList todoList = new TodoList();
+
+    Task task1 = new Task("Go shopping");
+    Task task2 = new Task("Make dinner");
+
+    @BeforeEach
+    public void setupTestEnvironment() {
+        todoList.clear();
     }
+
+    @Test
+    public void canInitializeTodoList() {
+        Assertions.assertEquals(new ArrayList<Task>(), todoList.getTasks());
+    }
+
+    @Test
+    public void canAddTaskToList() {
+        Assertions.assertTrue(todoList.add(task1));
+    }
+
 }
