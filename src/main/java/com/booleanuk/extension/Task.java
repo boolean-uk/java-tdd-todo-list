a5c9069d7c389701ad6c1a38dcc05a9f9a98e324
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-16 15:03:37 +0100

    Add necessary fields and constructor to task class

--- src/main/java/com/booleanuk/extension/Task.java
+++ src/main/java/com/booleanuk/extension/Task.java
@@ -1,5 +1,17 @@
 package com.booleanuk.extension;
 
-public class Task {
+import java.util.Date;
 
+public class Task {
+    String todoId;
+    String name;
+    boolean status;
+    Date creationTime;
+
+    public Task(String todoId, String name) {
+        this.todoId = todoId;
+        this.name = name;
+        this.status = false;
+        this.creationTime = new Date();
+    }
 }
