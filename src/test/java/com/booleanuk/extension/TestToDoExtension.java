05ab64eb8adc889c2685040bc17d286fd19bce9d
Maha Riazi <maha.riazi@hotmail.no>
2024-01-16 15:41:53 +0100

    added tests

--- src/test/java/com/booleanuk/extension/TestToDoExtension.java
+++ src/test/java/com/booleanuk/extension/TestToDoExtension.java
@@ -1,25 +1,46 @@
 package com.booleanuk.extension;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestToDoExtension {
     @Test
     public void testGetTaskByID(){
+        ToDoExtension todo = new ToDoExtension();
+        todo.add("abc1234", "brush hair");
+        todo.add("ooo3", "eat soup");
 
+        Assertions.assertEquals(todo.getTask("abc1234").description, "brush hair");
+        Assertions.assertEquals(todo.getTask("ooo3").description, "eat soup");
     }
 
     @Test
     public void testUpdateNameByID(){
+        ToDoExtension todo = new ToDoExtension();
+        todo.add("7646", "play golf");
 
+        Assertions.assertEquals(todo.getTask("7646").description, "play golf");
+
+        todo.changeName("7646", "play tennis");
+
+        Assertions.assertFalse(todo.getTask("7646").description.equals("play golf"));
+        Assertions.assertEquals(todo.getTask("7646").description, "play tennis");
     }
 
     @Test
     public void testChangeStatusByID(){
+        ToDoExtension todo = new ToDoExtension();
+        todo.add("123", "...");
 
+        Assertions.assertFalse(todo.getTask("123").status);
+
+        todo.changeStatus("123", true);
+
+        Assertions.assertTrue(todo.getTask("123").status);
     }
 
     @Test
     public void testDateAndTimeCreated(){
-
+        // TODO ============================================== test printed to screen
     }
 }
