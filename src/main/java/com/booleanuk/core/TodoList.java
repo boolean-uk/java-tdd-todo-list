4e64983fd5c2876d30dc546bc4436e10293ae15a
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-16 12:57:24 +0100

    Implement add method

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,20 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+
 public class TodoList {
 
+    HashMap<String, Boolean> todos;
+
+    public TodoList() {
+        this.todos = new HashMap<>();
+    }
+
+    public boolean add(String todo) {
+        if (todo.isBlank() || this.todos.containsKey(todo)) {
+            return false;
+        }
+        this.todos.put(todo, false);
+        return true;
+    }
 }
