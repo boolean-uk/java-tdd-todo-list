a0e73f7e6c4aa489b204fe6244e31236db059435
stp <steventphung6@gmail.com>
2024-01-16 13:10:04 +0100

    add tasks test

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -5,9 +5,19 @@ import org.junit.jupiter.api.Test;
 
 class TodoListTest {
     @Test
-    public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
+    public void testAdd() {
+        TodoList todoList = new TodoList();
+        String task = "Wash";
+        Assertions.assertTrue(todoList.add(task));
+    }
+
+    @Test
+    public void testAddExisting() {
+        TodoList todoList = new TodoList();
+        String task = "Wash";
+        String task2 = "Clean";
+        Assertions.assertTrue(todoList.add(task));
+        Assertions.assertTrue(todoList.add(task2));
+        Assertions.assertFalse(todoList.add(task));
     }
 }
