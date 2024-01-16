6bb2cef5928763dd0b8c52670fef961f83878879
Knut S Rostad <knut.rostad@no.experis.com>
2024-01-16 13:57:31 +0100

    Made domain model. Made tests that failed. Made empty methods in TodoList class

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,57 @@
 package com.booleanuk.core;
 
-public class TodoList {
+import java.util.ArrayList;
 
+public class TodoList {
+    public ArrayList<TodoList> taskList;
+    private String task;
+    private boolean status;
+
+    public TodoList(){
+        this.taskList = new ArrayList<>();
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void add(String task){
+
+    }
+
+    public String showAll(){
+
+        return "";
+    }
+
+    public void changeStatus(String task){
+
+    }
+
+    public String showCompletedTasks() {
+
+        return"";
+    }
+
+    public String showIncompleteTasks() {
+
+        return"";
+    }
+
+    public String search(String task){
+
+        return"";
+    }
+
+    public void removeTask(String task){
+
+    }
+
+    public void sortListAsc(){
+
+    }
+
+    public void sortListDesc(){
+        //Reverse sortListAsc();
+    }
 }
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -5,9 +5,76 @@ import org.junit.jupiter.api.Test;
 
 class TodoListTest {
     @Test
-    public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
+    public void testAddToList(){
+        TodoList toDoList = new TodoList();
+
+        toDoList.add("Get mail");
+        Assertions.assertEquals("Get mail",toDoList.taskList.get(toDoList.taskList.size()-1));
     }
+
+    @Test
+    public void testShowAll(){
+        TodoList toDoList = new TodoList();
+
+        Assertions.assertEquals("Task: Get mail, incomplete",toDoList.showAll());
+    }
+
+    @Test
+    public void testChangeStatus(){
+        TodoList toDoList = new TodoList();
+
+        toDoList.changeStatus("Get mail");
+        Assertions.assertTrue(toDoList.taskList.get(0).getStatus());
+    }
+
+    @Test
+    public void testShowCompletedTasks(){
+        TodoList toDoList = new TodoList();
+
+        toDoList.add("Go running");
+        toDoList.add("Check email");
+        toDoList.changeStatus("Go running");
+        Assertions.assertEquals("Task: Get mail, Complete, Task: Go running, Complete",toDoList.showCompletedTasks());
+    }
+
+    @Test
+    public void testShowIncompleteTasks(){
+        TodoList toDoList = new TodoList();
+
+        toDoList.add("Check fav yt");
+        Assertions.assertEquals("Task: Check email, Incomplete, Task: Check fav yt, Incomplete",toDoList.showIncompleteTasks());
+    }
+
+    @Test
+    public void testSearchNotExists(){
+        TodoList toDoList = new TodoList();
+
+        Assertions.assertEquals("Task: Get mail, Complete",toDoList.search("Get mail"));
+        Assertions.assertEquals("Task does not exist",toDoList.search("Look at the moon"));
+    }
+
+    @Test
+    public void testRemoveTask(){
+        TodoList toDoList = new TodoList();
+
+        toDoList.removeTask("Get mail");
+        Assertions.assertEquals("Task: Go running, Complete, Task: Check email, Incomplete, Task: Check fav yt, Incomplete",toDoList.showAll());
+    }
+
+    @Test
+    public void testSortListAsc(){
+        TodoList toDoList = new TodoList();
+
+        toDoList.sortListAsc();
+        Assertions.assertEquals("Task: Check email, Incomplete, Task: Check fav yt, Incomplete, Task: Go running, Complete",toDoList.showAll());
+    }
+    @Test
+    public void testSortListDesc(){
+        TodoList toDoList = new TodoList();
+
+        toDoList.sortListDesc();
+        Assertions.assertEquals("Task: Go running, Complete, Task: Check fav yt, Incomplete, Task: Check email, Incomplete",toDoList.showAll());
+    }
+
+
 }
