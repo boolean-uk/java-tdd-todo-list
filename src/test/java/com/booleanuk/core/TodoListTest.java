8e6acc0c0f7837f0abb3075edbd9711d4ab29dc8
Marit <marit.moe2@ntebb.no>
2024-01-16 14:03:02 +0100

    Tests for getTasks().

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -87,4 +87,22 @@ class TodoListTest {
         expectedOutput = "Laundry wasn't found.";
         Assertions.assertEquals(expectedOutput, todoList.getTask("Laundry"));
     }
+
+    @Test
+    public void testCorrectOutputFromGetTasks() {
+        TodoList todoList = new TodoList();
+        todoList.add("Vacuum");
+        todoList.add("Laundry");
+        todoList.add("Go for a walk");
+        todoList.changeStatus("Go for a walk");
+        String expectedOutput = "Vacuum incomplete\nLaundry incomplete\nGo for a walk complete";
+        Assertions.assertEquals(expectedOutput, todoList.getTasks());
+    }
+
+    @Test
+    public void testReturnEmptyStringFromGetTasks() {
+        TodoList todoList = new TodoList();
+        String expectedOutput = "";
+        Assertions.assertEquals(expectedOutput, todoList.getTasks());
+    }
 }
