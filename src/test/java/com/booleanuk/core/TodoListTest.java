b5ae49b4523f7b490218c08f2f588d49108706d6
Gustav Svennas <gustavsvennas@gmail.com>
2024-01-16 13:45:26 +0100

    New test for addTask()

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -3,11 +3,23 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 class TodoListTest {
     @Test
     public void exampleTest() {
         String hello = "Hello";
         Assertions.assertEquals("Hello", hello);
         Assertions.assertNotEquals("Goodbye", hello);
+    }
+
+    @Test
+    public void addOneNewTask() {
+        TodoList result = new TodoList();
+
+        HashMap<String, Boolean> expected = new HashMap<>();
+        expected.put("Do the dishes", false);
+
+        Assertions.assertEquals(expected, result.addTask());
     }
 }
