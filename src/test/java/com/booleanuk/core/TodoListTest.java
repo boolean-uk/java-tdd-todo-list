0591b6ee012ac3e833842b59758983263afea1ca
hanna <hannaphamm1007@gmail.com>
2024-01-16 14:10:34 +0100

    add test1

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -5,9 +5,14 @@ import org.junit.jupiter.api.Test;
 
 class TodoListTest {
     @Test
-    public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
+    public void addingTaskShouldReturnTrue() {
+        TodoList todoList = new TodoList();
+        Assertions.assertTrue(todoList.addTask("Task1", "incomplete"));
+    }
+    @Test
+    public void addingTaskShouldReturnFalse() {
+        TodoList todoList = new TodoList();
+        todoList.addTask("Task1", "incomplete");
+        Assertions.assertFalse(todoList.addTask("Task1", "incomplete"));
     }
 }
