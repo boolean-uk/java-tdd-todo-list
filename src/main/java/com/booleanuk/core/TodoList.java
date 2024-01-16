08ac6c493b39c1e06a8f59c30d8d9ff054cdc144
Marit <marit.moe2@ntebb.no>
2024-01-16 13:14:45 +0100

    Tests pass for add().

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,20 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+
 public class TodoList {
+    HashMap<String, Boolean> tasks;
+
+    public TodoList() {
+        this.tasks = new HashMap<>();
+    }
+
+    public boolean add(String task) {
+        if (tasks.containsKey(task)) {
+            return false;
+        }
+        this.tasks.put(task, false);
+        return true;
+    }
 
 }
