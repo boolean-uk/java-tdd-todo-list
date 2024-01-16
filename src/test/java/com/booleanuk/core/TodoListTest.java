fe7a7242eba7429bcaa3a5c32eb93fb71ca0462d
hanna <hannaphamm1007@gmail.com>
2024-01-16 15:22:54 +0100

    add test 3

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -3,6 +3,11 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
+
 class TodoListTest {
     @Test
     public void addingTaskShouldReturnTrue() {
@@ -15,4 +20,22 @@ class TodoListTest {
         todoList.addTask("Task1", "incomplete");
         Assertions.assertFalse(todoList.addTask("Task1", "incomplete"));
     }
+    @Test
+    public void shouldChangeStatusToComplete(){
+        TodoList todoList = new TodoList();
+        todoList.addTask("Task1", "incomplete");
+        todoList.changeStatus("Task1", "complete");
+        Assertions.assertEquals("complete", todoList.taskList.get("Task1"));
+    }
+    @Test
+    public void shouldChangeStatusToIncomplete(){
+        TodoList todoList = new TodoList();
+        todoList.addTask("Task1", "complete");
+        todoList.changeStatus("Task1", "incomplete");
+        Assertions.assertEquals("incomplete", todoList.taskList.get("Task1"));
+    }
+
+
+
+
 }
