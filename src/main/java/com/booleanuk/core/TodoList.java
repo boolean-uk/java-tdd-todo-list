c449edc9fcd9911ed646563880e25883558921b1
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-16 12:45:02 +0100

    addAlreadyExistingTaskTest passed

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,8 +1,19 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+
 public class TodoList {
 
+    HashMap<String, Boolean> tasks;
+
+    public TodoList() {
+        tasks = new HashMap<>();
+    }
     public boolean addTask(String task) {
+        if(tasks.containsKey(task)) {
+            return false;
+        }
+        tasks.put(task, false);
         return true;
     }
 
