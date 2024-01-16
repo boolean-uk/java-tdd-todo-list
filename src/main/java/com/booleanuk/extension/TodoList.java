9bb03b9ea9c8604496f4f724e603b7917598ba88
msg <mats.gravem@no.experis.com>
2024-01-16 14:18:41 +0100

    Extention - copied base files from core

---
+++ src/main/java/com/booleanuk/extension/TodoList.java
@@ -0,0 +1,60 @@
+package com.booleanuk.extension;
+
+import com.booleanuk.core.Task;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class TodoList {
+    private ArrayList<Task> tasks;
+
+    public TodoList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public boolean add(Task task) {
+        return tasks.add(task);
+    }
+
+    public boolean remove(Task task) {
+        return tasks.remove(task);
+    }
+
+    public Task getTask(String description) {
+        for (Task task : tasks) {
+            // Only exact match
+            if (task.getDescription().equals(description)) {
+                return task;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    public ArrayList<Task> getTasks(boolean completion) {
+        ArrayList<Task> tmpArray = new ArrayList<>();
+        for (Task task : tasks) {
+            if (task.isCompleted() == completion) {
+                tmpArray.add(task);
+            }
+        }
+        return tmpArray;
+    }
+
+    public ArrayList<Task> getAlphabeticalTasks(boolean ascending) {
+        ArrayList<Task> tmpArray = tasks;
+        tmpArray.sort(Comparator.comparing(Task::getDescription));
+        if (!ascending) {
+            Collections.reverse(tmpArray);
+        }
+        return tmpArray;
+    }
+
+    public void clear() {
+        this.tasks.clear();
+    }
+}
