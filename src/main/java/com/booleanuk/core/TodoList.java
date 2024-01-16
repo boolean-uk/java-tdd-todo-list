e512d928a07cecc24671645ddf367384713f7997
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-01-16 11:58:22 +0100

    Implemented addTask(

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,20 @@
 package com.booleanuk.core;
 
-public class TodoList {
+import java.util.HashMap;
 
+public class TodoList {
+    HashMap<String, Boolean> taskList;
+
+    public TodoList() {
+        taskList = new HashMap<>();
+    }
+
+    public boolean addTask(String task)  {
+        if(taskList.get(task) == null)
+        {
+            taskList.put(task, false);
+            return true;
+        }
+        return false;
+    }
 }
