cceea43f185be174601b8fa23b86b02755956d51
Osamah Al-maliki <osamah.al-maliki@no.experis.com>
2024-01-16 13:14:44 +0100

    Wrote method for adding tasks to list, now passing test

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,19 @@
 package com.booleanuk.core;
 
-public class TodoList {
+import java.util.ArrayList;
 
+public class TodoList {
+    public ArrayList<String> todoList;
+
+    public TodoList() {
+        this.todoList = new ArrayList<>();
+    }
+
+    public boolean add(String task) {
+        if (task == null) {
+            return false;
+        } else {
+            return todoList.add(task);
+        }
+    }
 }
