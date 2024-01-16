51ff498793c968ba636a5feb9cb1f45f5d867b04
Marit <marit.moe2@ntebb.no>
2024-01-16 13:10:06 +0100

    Tests for add().

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -10,4 +10,26 @@ class TodoListTest {
         Assertions.assertEquals("Hello", hello);
         Assertions.assertNotEquals("Goodbye", hello);
     }
+
+    @Test
+    public void returnTrueAddTaskNotInList() {
+        TodoList todoList = new TodoList();
+        Assertions.assertTrue(todoList.add("Vacuum"));
+    }
+
+    @Test
+    public void returnFalseAddTaskAlreadyInList() {
+        TodoList todoList = new TodoList();
+        todoList.add("Vacuum");
+        Assertions.assertFalse(todoList.add("Vacuum"));
+    }
+
+    @Test
+    public void addingMultipleTasksWorkCorrectly() {
+        TodoList todoList = new TodoList();
+        Assertions.assertTrue(todoList.add("Vacuum"));
+        Assertions.assertTrue(todoList.add("Laundry"));
+        Assertions.assertTrue(todoList.add("Go for a walk"));
+        Assertions.assertFalse(todoList.add("Laundry"));
+    }
 }
