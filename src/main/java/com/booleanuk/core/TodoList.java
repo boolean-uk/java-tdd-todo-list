7dd9c34c175845b3b9466c0e052873d2d84d8863
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-01-16 13:18:43 +0100

    Added showAllTasks() and stored tasks in list to preserve order tasks are added

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,20 +1,52 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class TodoList {
-    HashMap<String, Boolean> taskList;
+    HashMap<String, Boolean> taskStatus;
+    ArrayList<String> taskList;
 
     public TodoList() {
-        taskList = new HashMap<>();
+        taskStatus = new HashMap<>();
+        taskList = new ArrayList<>();
     }
 
     public boolean addTask(String task)  {
-        if(taskList.get(task) == null)
+        if(!taskList.contains(task))
         {
-            taskList.put(task, false);
+            taskList.add(task);
+            taskStatus.put(task, false);
             return true;
         }
         return false;
+    }
+
+    /*
+    expectedString = """
+        Your tasks are:
+        [ ] Eat yoghurt
+        [ ] Paint the Mona Lisa
+        [ ] Do laundry
+        [ ] Talk to janitor
+        """;
+     */
+    public void showAllTasks()   {
+        if(!taskList.isEmpty()) {
+            StringBuilder outputString = new StringBuilder();
+            outputString.append("Your tasks are:\n");
+            for(String task : taskList)  {
+                outputString.append("[");
+                outputString.append(!taskStatus.get(task) ? " " : "X");
+                outputString.append(("] "));
+                outputString.append(task);
+                outputString.append("\n");
+            }
+            System.out.print(outputString.toString());
+        }   else {
+            System.out.print("No tasks to display, todo list is empty");
+        }
+
     }
 }
