a2f17eadafcbfc171723341fcaad07b4029d53d9
hanna <hannaphamm1007@gmail.com>
2024-01-16 14:16:22 +0100

    add function 1

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,23 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+
 public class TodoList {
+    public HashMap<String, String> taskList;
+
+    public TodoList(){
+        this.taskList = new HashMap<>();
+    }
+
+    public boolean addTask(String task, String status){
+        if(taskList.containsKey(task)){
+            System.out.println("Task already exits");
+            return false;
+        }
+        taskList.put(task, status);
+        System.out.println("Task added successfully!");
+        return true;
+    }
+
 
 }
