58e10b5128384d00fc97473f62cc848349d6d43e
Petter Karstensen <petterkarstensen@gmail.com>
2024-01-16 12:32:29 +0100

    Init TodoList

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,12 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class TodoList {
+    ArrayList<String> tasks;
+
+    public TodoList(){
+        this.tasks = new ArrayList<>();
+    }
 
 }
