abf9ced3bb965ffb137cba53fb95dda986b263a0
hanna <hannaphamm1007@gmail.com>
2024-01-16 15:29:16 +0100

    add function 2,3

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,6 +1,7 @@
 package com.booleanuk.core;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class TodoList {
     public HashMap<String, String> taskList;
@@ -15,8 +16,32 @@ public class TodoList {
             return false;
         }
         taskList.put(task, status);
-        System.out.println("Task added successfully!");
         return true;
+    }
+    public void getAllTask(){
+        if(taskList.isEmpty()){
+            System.out.println("There is no task left");
+        }
+        for (Map.Entry<String, String> entry : taskList.entrySet()){
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+    public void changeStatus(String task, String status){
+        if(taskList.containsKey(task)){
+            taskList.put(task,status);
+        }
+
+    }
+    public static void main(String[] arg){
+        TodoList object = new TodoList();
+        object.addTask("Task1", "incomplete");
+        object.addTask("Task2", "incomplete");
+        object.getAllTask();
+        object.changeStatus("Task1", "complete");
+        object.changeStatus("Task2", "complete");
+        object.getAllTask();
+
+
     }
 
 
