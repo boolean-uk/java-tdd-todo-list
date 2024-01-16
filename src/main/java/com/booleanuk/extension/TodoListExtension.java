c40a2be489e77674f9dc447c17b36e2dfa37466c
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-16 15:05:06 +0100

    Add necessary fields and constructor to TodoListExtension class

--- src/main/java/com/booleanuk/extension/TodoListExtension.java
+++ src/main/java/com/booleanuk/extension/TodoListExtension.java
@@ -1,4 +1,13 @@
 package com.booleanuk.extension;
 
+import java.util.ArrayList;
+
 public class TodoListExtension {
+    ArrayList<Task> todoList;
+
+    public TodoListExtension() {
+        this.todoList = new ArrayList<>();
+    }
+
+
 }
