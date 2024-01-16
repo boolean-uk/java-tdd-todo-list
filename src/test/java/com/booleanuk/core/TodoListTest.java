891bdffb860627536f4ce33cbf1031943154124d
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-16 13:28:43 +0100

    Add test for checking printTasksFromTodolist

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -1,12 +1,38 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
 
 class TodoListTest {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    private String getOutput() {
+        return outputStreamCaptor.toString().trim();
+    }
+
     @Test
     public void exampleTest() {
         String hello = "Hello";
@@ -16,7 +42,7 @@ class TodoListTest {
 
 
     @Test
-    public void TestAddTaskToTodolist() {
+    public void testAddTaskToTodolist() {
         TodoList todoList = new TodoList();
 
         todoList.addTaskToTodolist("Task1");
@@ -29,6 +55,22 @@ class TodoListTest {
         todoList.addTaskToTodolist(null);
 
         Assertions.assertFalse(todolistMap.containsKey(null));
+
+    }
+
+    @Test
+    public void testPrintTasksFromTodolist() {
+        TodoList todoList = new TodoList();
+
+        todoList.addTaskToTodolist("Task1");
+        todoList.addTaskToTodolist("Task2");
+        todoList.addTaskToTodolist("Task3");
+        todoList.addTaskToTodolist("Task4");
+
+
+        Map<String, Boolean> todolistMap = todoList.getTodolist();
+        todoList.printTasksFromTodolist();
+        Assertions.assertEquals("Task1\nTask2\nTask3\nTask4", getOutput());
 
     }
 
