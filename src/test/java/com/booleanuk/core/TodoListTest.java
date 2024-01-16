0c1248e5e1c6fe46960ba81db98c9378b6ee9e89
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-01-16 13:05:14 +0100

    Added test for checknig output of showAllTasks()

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -1,9 +1,27 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 class TodoListTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown()  {
+        System.setOut(standardOut);
+    }
+
     @Test
     public void exampleTest() {
         String hello = "Hello";
@@ -36,5 +54,26 @@ class TodoListTest {
         Assertions.assertTrue(todoList.addTask("Talk to janitor"));
         Assertions.assertFalse(todoList.addTask("Eat yoghurt"));
         Assertions.assertTrue(todoList.addTask("Sing a song"));
+    }
+
+    @Test
+    public void testShowAllTasks()  {
+        TodoList todoList = new TodoList();
+
+        todoList.addTask("Eat yoghurt");
+        todoList.addTask("Paint the Mona Lisa");
+        todoList.addTask("Do laundry");
+        todoList.addTask("Talk to janitor");
+
+        String expectedString = """
+                Your tasks are:
+                [ ] Eat yoghurt
+                [ ] Paint the Mona Lisa
+                [ ] Do laundry
+                [ ] Talk to janitor
+                """;
+        todoList.showAllTasks();
+
+        Assertions.assertEquals(expectedString, outputStreamCaptor.toString());
     }
 }
