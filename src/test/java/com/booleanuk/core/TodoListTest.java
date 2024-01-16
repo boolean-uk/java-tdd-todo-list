4dc4031f9417b5681ca88158f6942890c23d1086
Axel Hansson <axel.co.hansson@gmail.com>
2024-01-16 14:36:32 +0100

    Test ShowTasks and ShowTasksWhenEmpty written

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -3,6 +3,8 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 class TodoListTest {
     @Test
     public void testAdd() {
@@ -13,5 +15,20 @@ class TodoListTest {
         Assertions.assertEquals("Task already in list", todolist.add("Do laundry"));
         todolist.add("Go fishing");
         Assertions.assertEquals("Go fishing", todolist.getTask("Go fishing"));
+    }
+
+    public void testShowTasks() {
+        TodoList todolist = new TodoList();
+        ArrayList<String> testList = new ArrayList<>();
+        testList.add("Do laundry");
+        testList.add("Go fishing");
+        todolist.add("Do laundry");
+        todolist.add("Go fishing");
+        Assertions.assertEquals(testList, todolist.getToDos());
+    }
+    public void testShowTasksWhenEmpty() {
+        TodoList todolist = new TodoList();
+        ArrayList<String> testList = new ArrayList<>();
+        Assertions.assertEquals(testList, todolist.getToDos());
     }
 }
