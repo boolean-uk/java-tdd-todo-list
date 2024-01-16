0fc9486b091c7fa9299c7428d51cecfaa6a13e4d
Enock97 <enockladu@hotmail.com>
2024-01-16 12:04:35 +0100

    Written test for user story 2 (red)

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -20,4 +20,16 @@ class TodoListTest {
         Assertions.assertEquals("Task added to todo list.", result);
     }
 
+    @Test
+    public void showTasksInTodoList() {
+        TodoList todoList = new TodoList();
+
+        todoList.add("Alice");
+        todoList.add("Bob");
+        todoList.add("Charlie");
+
+        String result = todoList.showTasks();
+
+        Assertions.assertEquals("[Alice, Bob, Charlie]", result);
+    }
 }
