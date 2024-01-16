2d951d68e295887ed0f6f468c30e5ebac235e1c2
Ateeb <ateeb@live.no>
2024-01-16 13:08:33 +0100

    Ateeb Salam

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -2,4 +2,6 @@ package com.booleanuk.core;
 
 public class TodoList {
 
+
+
 }
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -10,4 +10,11 @@ class TodoListTest {
         Assertions.assertEquals("Hello", hello);
         Assertions.assertNotEquals("Goodbye", hello);
     }
+    @Test
+    public void testAddTaskToList() {
+        Main main = new Main();
+        main.add("clean");
+        Assertions.assertEquals("clean",main.toDoList.Contain("clean"));
+
+    }
 }
