9e845554ca1e48bded66703026aa8b92b3dcb7cc
Espen Solhaug <espen.solhaug@no.experis.com>
2024-01-16 14:05:25 +0100

    Added base testing, empty methods and Task class

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,44 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class TodoList {
 
+    public ArrayList<Task> tasks;
+
+    public TodoList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public Task addTask(String name) {
+        return new Task("a");
+    }
+
+    public String showTasks(ArrayList<Task> tasksToPrint) {
+        return "";
+    }
+
+    public String showCompleteTasks() {
+        return "";
+    }
+
+    public String showIncompleteTasks() {
+        return "";
+    }
+
+    public String search(String name) {
+        return "";
+    }
+
+    public boolean delete(String name) {
+        return false;
+    }
+
+    public ArrayList<Task> orderTasksAsc() {
+        return new ArrayList<>();
+    }
+
+    public ArrayList<Task> orderTasksDesc() {
+        return new ArrayList<>();
+    }
 }
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -3,11 +3,99 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class TodoListTest {
+
+    TodoList todoList = new TodoList();
+
     @Test
-    public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
+    public void testAddTask() {
+        Assertions.assertInstanceOf(Task.class, todoList.addTask("Dishes"));
+        Assertions.assertEquals(todoList.tasks.get(1), todoList.addTask("Vacuum"));
     }
+
+    @Test
+    public void testShowTasks() {
+        Task[] array = new Task[] { new Task("Dishes"), new Task("Vacuum") };
+        todoList.tasks = (ArrayList<Task>) List.of(array);
+        String tasksString = todoList.showTasks(todoList.tasks);
+        Assertions.assertTrue(tasksString.contains("Dishes"));
+        Assertions.assertTrue(tasksString.contains("Incomplete"));
+    }
+
+    @Test
+    public void testSetStatus() {
+        Task task = new Task("Dishes");
+        Assertions.assertFalse(task.getStatus());
+        task.setStatus(true);
+        Assertions.assertTrue(task.getStatus());
+    }
+
+    @Test
+    public void testShowCompleteTasks() {
+        Task[] array = new Task[] { new Task("Dishes", false), new Task("Vacuum", true), new Task("Empty Trash", false), new Task("Walk the dog", true) };
+        todoList.tasks = (ArrayList<Task>) List.of(array);
+        String completedTasksString = todoList.showCompleteTasks();
+        Assertions.assertTrue(completedTasksString.contains("Vacuum"));
+        Assertions.assertTrue(completedTasksString.contains("Walk the dog"));
+        Assertions.assertFalse(completedTasksString.contains("Dishes"));
+        Assertions.assertFalse(completedTasksString.contains("Empty Trash"));
+    }
+
+    @Test
+    public void testShowIncompleteTasks() {
+        Task[] array = new Task[] { new Task("Dishes", false), new Task("Vacuum", true), new Task("Empty Trash", false), new Task("Walk the dog", true) };
+        todoList.tasks = (ArrayList<Task>) List.of(array);
+        String completedTasksString = todoList.showIncompleteTasks();
+        Assertions.assertTrue(completedTasksString.contains("Empty trash"));
+        Assertions.assertTrue(completedTasksString.contains("Dishes"));
+        Assertions.assertFalse(completedTasksString.contains("Vacuum"));
+        Assertions.assertFalse(completedTasksString.contains("Walk the dog"));
+    }
+
+    @Test
+    public void testSearchForNameFound() {
+        Task[] array = new Task[] { new Task("Dishes"), new Task("Vacuum"), new Task("Empty Trash"), new Task("Walk the dog") };
+        todoList.tasks = (ArrayList<Task>) List.of(array);
+        String foundMessage = todoList.search("Dishes");
+        Assertions.assertEquals("Dishes was found", foundMessage);
+    }
+
+    @Test
+    public void testSearchForNameNotFound() {
+        Task[] array = new Task[] { new Task("Dishes"), new Task("Vacuum"), new Task("Empty Trash"), new Task("Walk the dog") };
+        todoList.tasks = (ArrayList<Task>) List.of(array);
+        String notFoundMessage = todoList.search("Wash floor");
+        Assertions.assertEquals("Wash floor was not found", notFoundMessage);
+    }
+
+    @Test
+    public void testDeleteExistingTask() {
+        Task[] array = new Task[] { new Task("Dishes"), new Task("Vacuum"), new Task("Empty Trash"), new Task("Walk the dog") };
+        todoList.tasks = (ArrayList<Task>) List.of(array);
+        boolean deleted = todoList.delete("Dishes");
+        Assertions.assertTrue(deleted);
+    }
+
+    @Test
+    public void testOrderTasksAsc() {
+        Task[] array = new Task[] { new Task("Dishes"), new Task("Vacuum"), new Task("Empty Trash"), new Task("Walk the dog") };
+        todoList.tasks = (ArrayList<Task>) List.of(array);
+        ArrayList<Task> sortedAsc = todoList.orderTasksAsc();
+        Assertions.assertEquals("Dishes", sortedAsc.get(0).getName());
+        Assertions.assertEquals("Empty trash", sortedAsc.get(1).getName());
+    }
+
+    @Test
+    public void testOrderTasksDesc() {
+        Task[] array = new Task[] { new Task("Dishes"), new Task("Vacuum"), new Task("Empty Trash"), new Task("Walk the dog") };
+        todoList.tasks = (ArrayList<Task>) List.of(array);
+        ArrayList<Task> sortedAsc = todoList.orderTasksDesc();
+        Assertions.assertEquals("Walk the dog", sortedAsc.get(0).getName());
+        Assertions.assertEquals("Vacuum", sortedAsc.get(1).getName());
+    }
+
 }
