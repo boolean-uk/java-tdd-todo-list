d2d48380cc81aa11bc60014679ebb29070f5e204
Alexander Niklasson <alexander.niklasson@live.com>
2024-01-16 13:24:58 +0100

    Added code to pass test

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,8 +1,31 @@
 package com.booleanuk.core;
 
-public class TodoList {
+import java.util.ArrayList;
 
+public class TodoList {
+    ArrayList<Task> tasks;
+
+    public TodoList(){
+        tasks = new ArrayList<>();
+    }
     public boolean addTask(Task task){
+        boolean taskExists;
+        if(taskExists(task)){
+            return false;
+        }
+        tasks.add(task);
         return true;
+    }
+    public boolean taskExists(Task task){
+        boolean exists = false;
+        if(!tasks.isEmpty()){
+            for(Task t: tasks){
+                if(t.getName().equals(task.getName())){
+                    exists = true;
+                    break;
+                }
+            }
+        }
+        return exists;
     }
 }
