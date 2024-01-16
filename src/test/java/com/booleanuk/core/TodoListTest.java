540d1842465bdd98f1133c40f7de2c26c18b1e04
noahlenn <noahlenn@hotmail.com>
2024-01-16 13:11:29 +0100

    TodoListTest 1

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -5,9 +5,18 @@ import org.junit.jupiter.api.Test;
 
 class TodoListTest {
     @Test
-    public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
+    public void testAddingTask1() {
+        TodoList todo = new TodoList();
+
+        Assertions.assertTrue(todo.list.addToTodo("clean"));
+
     }
+
+    @Test
+    public void testAddingTask2() {
+        TodoList todo = new TodoList();
+
+        Assertions.assertTrue(todo.list.addToTodo("wash"));
+    }
+
 }
