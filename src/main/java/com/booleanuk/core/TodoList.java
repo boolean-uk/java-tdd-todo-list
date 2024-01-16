549b04a5528b90f69cf1866858b0eeb35ec73c87
Giar Rehani <giarreh1@gmail.com>
2024-01-16 13:14:41 +0100

    Source: add, TodoList, Task class

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,34 @@
 package com.booleanuk.core;
 
-public class TodoList {
+import java.util.ArrayList;
 
+public class TodoList {
+    public static class Task{
+        String description;
+        boolean isCompleted;
+        public Task(String task){
+            this.description = task;
+            this.isCompleted = false;
+        }
+    }
+    ArrayList<Task> list;
+    public TodoList(){
+        this.list = new ArrayList<>();
+    }
+
+    public boolean add(String taskDescription){
+        if(taskDescription.isEmpty()){
+            return false;
+        }
+        Task task = new Task(taskDescription);
+        list.add(task);
+        return true;
+    }
+
+    public static void main(String[] args) {
+        TodoList t = new TodoList();
+        t.add("Need to brush my teeth");
+
+        //System.out.println(t.list.get(0).description + t.list.get(0).isCompleted);
+    }
 }
