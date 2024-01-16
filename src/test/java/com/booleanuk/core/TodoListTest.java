93851ee14b82ed0151ae4dd29405ca328cbe9e06
Hiko Yüksek <33344671+hkyksk@users.noreply.github.com>
2024-01-16 22:59:37 +0100

    Red: initiate tests

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -5,9 +5,52 @@ import org.junit.jupiter.api.Test;
 
 class TodoListTest {
     @Test
-    public void exampleTest() {
+    public void addTaskGetAllTasks() {
         String hello = "Hello";
         Assertions.assertEquals("Hello", hello);
         Assertions.assertNotEquals("Goodbye", hello);
     }
+    @Test
+    public void returAllCompletedTasks() {
+        String hello = "Hello";
+        Assertions.assertEquals("Hello", hello);
+        Assertions.assertNotEquals("Goodbye", hello);
+    }
+    @Test
+    public void returAllIncompletedTasks(){
+        String hello = "Hello";
+        Assertions.assertEquals("Hello", hello);
+        Assertions.assertNotEquals("Goodbye", hello);
+    }
+
+    @Test
+    void searchTask() {
+        String hello = "Hello";
+        Assertions.assertEquals("Hello", hello);
+        Assertions.assertNotEquals("Goodbye", hello);
+    }
+
+    @Test
+    void removeTask() {
+        String hello = "Hello";
+        Assertions.assertEquals("Hello", hello);
+        Assertions.assertNotEquals("Goodbye", hello);
+    }
+
+    @Test
+    void getAlphabeticalTasks() {
+        String hello = "Hello";
+        Assertions.assertEquals("Hello", hello);
+        Assertions.assertNotEquals("Goodbye", hello);
+    }
+
+    @Test
+    void changeTaskStatus() {
+        String hello = "Hello";
+        Assertions.assertEquals("Hello", hello);
+        Assertions.assertNotEquals("Goodbye", hello);
+    }
+
+
+
 }
