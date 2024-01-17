91808935e74191d206902d446ac72caaca0bab6d
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-17 08:45:34 +0100

    Add more to tests

--- src/test/java/com/booleanuk/extension/TaskTest.java
+++ src/test/java/com/booleanuk/extension/TaskTest.java
@@ -7,16 +7,25 @@ public class TaskTest {
 
     @Test
     public void testGetDate() {
+        Task task = new Task();
 
+        task.getTaskDate();
     }
 
     @Test
     public void testGetStatus() {
 
+        Task task = new Task();
+
+        task.getStatus("Task1");
+
     }
 
     @Test
     public void testGetSetStatus() {
+        Task task = new Task();
+
+        task.setStatusOfTask("Task");
 
     }
 }
