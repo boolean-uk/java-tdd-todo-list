dfe68cb5527c93cb4bd89ba7ff15f8b7ccf54295
Hiko Yüksek <33344671+hkyksk@users.noreply.github.com>
2024-01-17 18:23:43 +0100

    Extension; Red: Add minimal test

---
+++ src/main/java/com/booleanuk/extension/TodoListExtended.java
@@ -0,0 +1,103 @@
+package com.booleanuk.extension;
+
+// import javax.print.attribute.standard.DateTimeAtCreation;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
+class Task {
+    private int id;
+    private String title;
+    private String description;
+    private boolean status;
+    private LocalDateTime creationDateTime;
+
+    public Task(int id, String title, String description, Boolean status) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.creationDateTime = LocalDateTime.now();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    public String toString() {
+        return "Task{id=" + id + ", title='" + title + "', description='" + description + "', status='" + status + "'}";
+    }
+
+}
+
+class TodoList {
+    private List<Task> tasks = new ArrayList<>();
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return tasks;
+    }
+
+
+    public List<Task> getCompleteTasks() {
+        List<Task> completeTasks = new ArrayList<>();
+        for (Task task : tasks) {
+            if (task.getStatus()) {
+                completeTasks.add(task);
+            }
+        }
+        return completeTasks;
+    }
+
+    public List<Task> getIncompleteTasks() {
+        List<Task> incompleteTasks = new ArrayList<>();
+        for (Task task : tasks) {
+            if (!task.getStatus()) {
+                incompleteTasks.add(task);
+            }
+        }
+        return incompleteTasks;
+    }
+
+    public Task searchTask(String title) {
+        for (Task task : tasks) {
+            if (task.getTitle().equals(title)) {
+                return task;
+            }
+        }
+        return null;
+    }
+
+    public void removeTask(Task task) {
+        tasks.remove(task);
+    }
+
+    public List<Task> getAlphabeticalTasks(boolean ascending) {
+        tasks.sort(Comparator.comparing(Task::getTitle));
+        ArrayList<Task> sortedTasks = new ArrayList<>(tasks);
+
+        if (!ascending) {
+            Collections.reverse(sortedTasks);
+        }
+
+        return sortedTasks;
+    }
+
+    public void changeTaskStatus(Task task, Boolean status) {
+        task.setStatus(status);
+    }
+}
