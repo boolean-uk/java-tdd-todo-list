66ad8fb56c496b727aef0a74b1e9627cc8933a9c
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-17 09:00:40 +0100

    Add test for getting date

--- src/main/java/com/booleanuk/extension/Task.java
+++ src/main/java/com/booleanuk/extension/Task.java
@@ -20,4 +20,7 @@ public class Task {
     public void setStatusOfTask(boolean b) {
 
     }
+
+
+
 }
--- src/test/java/com/booleanuk/extension/TaskTest.java
+++ src/test/java/com/booleanuk/extension/TaskTest.java
@@ -1,17 +1,25 @@
 package com.booleanuk.extension;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
+
+import static java.lang.Thread.sleep;
 
 public class TaskTest {
 
 
     @Test
-    public void testGetDate() {
+    public void testGetDate() throws InterruptedException {
         Task task = new Task();
 
+
         Date date1 = task.getTaskDate();
+        sleep(2000);
+        Date date2 = task.getTaskDate();
+
+        Assertions.assertEquals(date1.getTime(), date2.getTime());
     }
 
     @Test
@@ -29,5 +37,8 @@ public class TaskTest {
 
         task.setStatusOfTask(true);
 
+
     }
+
+
 }
