1f2abc1072f8736e2bc7b17f1808b5ee312b1605
Alexander <alexander.ellnestam@gmail.com>
2024-01-17 08:35:48 +0100

    Tests

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -3,11 +3,31 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class TodoListTest {
+class ToDoListTest {
+
     @Test
-    public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
+    public void canAddTask() {
+        TodoList todoList = new TodoList();
+        todoList.add("Task");
+        Assertions.assertTrue(todoList.add("Task"));
+
     }
+
+    @Test
+    public void canRemove(){
+        TodoList todoList = new TodoList();
+        todoList.add("Task");
+        Assertions.assertTrue(todoList.remove("Task"));
+        Assertions.assertFalse(todoList.ts.containsKey("Task"));
+
+    }
+
+    @Test
+    public void changeStatusTasks(){
+        TodoList todoList = new TodoList();
+        todoList.add("Run");
+        Assertions.assertTrue(todoList.setStatus("Run"));
+    }
+
+
 }
