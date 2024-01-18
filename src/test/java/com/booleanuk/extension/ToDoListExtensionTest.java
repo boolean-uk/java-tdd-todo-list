fe293b1db50396d85d32f0932727d737eaca0208
Ateeb <ateeb@live.no>
2024-01-18 23:34:06 +0100

    Ateeb Salam

--- src/test/java/com/booleanuk/extension/ToDoListExtensionTest.java
+++ src/test/java/com/booleanuk/extension/ToDoListExtensionTest.java
@@ -2,7 +2,7 @@ package com.booleanuk.extension;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ToDoListExtensionTest {
     private TodoListExtension todoList;
@@ -13,5 +13,17 @@ public class ToDoListExtensionTest {
         todoList.addTask(taskName);
         String expected = "ID: 1, Task: Test Task, Status: incomplete";
         assertEquals(expected, todoList.getTaskById(1));
+    }
+
+    @Test
+    void testUpdateTaskName() {
+        todoList = new TodoListExtension();
+        todoList.addTask("walk");
+        assertTrue(todoList.updateTaskName(1, "run"));
+
+        String expected = "ID: 1, Task: run, Status: incomplete";
+        assertEquals(expected, todoList.getTaskById(1));
+
+        assertFalse(todoList.updateTaskName(99, "Non-existent Task"));
     }
 }
