576089e34194c2f8fab15751b4c879302cbf3558
Ateeb <ateeb@live.no>
2024-01-18 23:43:38 +0100

    Ateeb Salam

--- src/test/java/com/booleanuk/extension/ToDoListExtensionTest.java
+++ src/test/java/com/booleanuk/extension/ToDoListExtensionTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ToDoListExtensionTest {
     private TodoListExtension todoList;
     @Test
-    void testGetTaskById() {
+    public void testGetTaskById() {
         todoList = new TodoListExtension();
         String taskName = "Test Task";
         todoList.addTask(taskName);
@@ -16,7 +16,7 @@ public class ToDoListExtensionTest {
     }
 
     @Test
-    void testUpdateTaskName() {
+    public void testUpdateTaskName() {
         todoList = new TodoListExtension();
         todoList.addTask("walk");
         assertTrue(todoList.updateTaskName(1, "run"));
@@ -25,5 +25,17 @@ public class ToDoListExtensionTest {
         assertEquals(expected, todoList.getTaskById(1));
 
         assertFalse(todoList.updateTaskName(99, "Non-existent Task"));
+    }
+    @Test
+    public void testChangeStatus(){
+        todoList = new TodoListExtension();
+        todoList.addTask("walk");
+        int taskId = 1;
+
+        String result1 = todoList.changeStatus(taskId);
+        assertEquals("Status changed to: complete", result1);
+
+        String result2 = todoList.changeStatus(taskId);
+        assertEquals("Status changed to: incomplete", result2);
     }
 }
