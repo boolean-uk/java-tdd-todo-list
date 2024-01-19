142b2f4ec88a51c877303ad4aedd2f54714141d2
Marit <marit.moe2@ntebb.no>
2024-01-19 16:02:22 +0100

    Pass test add().

--- src/main/java/com/booleanuk/extension/TodoListExtension.java
+++ src/main/java/com/booleanuk/extension/TodoListExtension.java
@@ -8,13 +8,24 @@ public class TodoListExtension {
     private HashMap<Integer, String> tasks;
     private HashMap<Integer, Boolean> taskStatus;
     private HashMap<Integer, LocalDateTime> dateTimes;
+    private int idCount;
 
     public TodoListExtension() {
-
+        this.tasks = new HashMap<>();
+        this.taskStatus = new HashMap<>();
+        this.dateTimes = new HashMap<>();
+        this.idCount = 0;
     }
 
     public void add(String task) {
+        this.idCount += 1;
+        this.tasks.put(this.idCount, task);
+        this.taskStatus.put(this.idCount, false);
+        this.dateTimes.put(this.idCount, LocalDateTime.now());
+    }
 
+    public HashMap<Integer, String> getTasks() {
+        return this.tasks;
     }
 
     public String getTask(int id) {
