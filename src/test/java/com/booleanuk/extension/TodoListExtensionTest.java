e5ad1586cea51ac1c4c216be082b5d9a34ba3f42
Marit <marit.moe2@ntebb.no>
2024-01-19 15:55:57 +0100

    Test add().

--- src/test/java/com/booleanuk/extension/TodoListExtensionTest.java
+++ src/test/java/com/booleanuk/extension/TodoListExtensionTest.java
@@ -3,6 +3,8 @@ package com.booleanuk.extension;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 public class TodoListExtensionTest {
     @Test
     public void exampleTest() {
@@ -13,6 +15,14 @@ public class TodoListExtensionTest {
 
     @Test
     public void add() {
+        TodoListExtension todoList = new TodoListExtension();
+        Assertions.assertEquals(new HashMap<>(), todoList.getTasks());
+        todoList.add("Vacuum");
+        Assertions.assertEquals(1, todoList.getTasks().size());
+        todoList.add("Laundry");
+        Assertions.assertEquals(2, todoList.getTasks().size());
+        todoList.add("Vacuum");
+        Assertions.assertEquals(3, todoList.getTasks().size());
     }
 
     @Test
