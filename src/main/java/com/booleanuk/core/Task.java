edee0f55016862ca423c2f4be8b3ff6e9d4fb73b
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-01-23 17:13:55 +0100

    Created method addTask with default return

--- src/main/java/com/booleanuk/core/Task.java
+++ src/main/java/com/booleanuk/core/Task.java
@@ -1,4 +1,11 @@
 package com.booleanuk.core;
 
 public class Task {
+    private String name;
+    private boolean completed;
+
+    public Task(String name) {
+        this.name = name;
+        completed = false;
+    }
 }
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -4,17 +4,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TodoListTest {
-    @Test
-    public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
-    }
 
     //User story 1
     @Test
-    public void returnTrueIfTaskIsCreated() {
-        boolean created = new Task("Buy groceries");
-        Assertions.assertTrue(created);
+    public void returnTrueIfTaskIsAdded() {
+        Task task = new Task("Buy groceries");
+        TodoList todoList = new TodoList();
+        boolean added = todoList.addTask(task);
+        Assertions.assertTrue(added);
     }
 }
