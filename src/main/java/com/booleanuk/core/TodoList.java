68edf52c94c63ff79974fa3ba480c861067f1385
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-01-23 17:15:35 +0100

    Implemented method addTask

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,8 +1,16 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class TodoList {
+    ArrayList<Task> tasks;
+
+    public TodoList() {
+        tasks = new ArrayList<>();
+    }
 
     public boolean addTask(Task task) {
+        tasks.add(task);
         return true;
     }
 }
